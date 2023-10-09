7a043067807685d602cb24aa9d50a2bcce967208
TranTanDat1162 <dattt11062002@gmail.com>
2023-10-09 21:16:11 +0700

    + Cập nhật Signup

---
+++ app/src/main/java/edu/uef/doan/SignupActivity.java
@@ -0,0 +1,39 @@
+package edu.uef.doan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SignupActivity extends AppCompatActivity {
+
+    TextView tvLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        addControl();
+        addEvent();
+    }
+
+    private void addEvent() {
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void addControl() {
+        tvLogin = findViewById(R.id.tvLogin);
+    }
+}
