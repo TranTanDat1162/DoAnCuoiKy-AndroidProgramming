02d3a8da928fb0c48376899c19273dd37b8eb167
TranTanDat1162 <dattt11062002@gmail.com>
2023-10-09 20:15:55 +0700

    + Xong được 2 giao diện Splash Screen và Login

---
+++ app/src/main/java/edu/uef/doan/SplashActivity.java
@@ -0,0 +1,50 @@
+package edu.uef.doan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    TextView app_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        imageView = findViewById(R.id.imageView);
+        app_name = findViewById(R.id.textView);
+
+        imageView.setAnimation(topAnim);
+        app_name.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this,LoginActivity.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair(imageView, "splash_image");
+                pairs[1] = new Pair(imageView, "splash_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this,pairs);
+                startActivity(i,options.toBundle());
+            }
+        }, 3000);
+    }
+}
