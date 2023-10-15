10a743c227d9f36cd9d2d1d0ec4ff558303e3e1a
nghia0410 <nghiabh20@uef.edu.vn>
2023-10-15 16:28:50 +0700

    ViewBaiTap,CreateBaiTap
    popup, chuyen trang

---
+++ app/src/main/java/edu/uef/doan/CreateBaiTap.java
@@ -0,0 +1,31 @@
+package edu.uef.doan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class CreateBaiTap extends AppCompatActivity {
+
+    AppCompatButton btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_bai_tap);
+
+        btn1 =  findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CreateBaiTap.this,"Tao bai thanh cong " ,Toast.LENGTH_SHORT).show();
+
+                Intent intent=new Intent(CreateBaiTap.this,HistoryFragment.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edu/uef/doan/ViewBaiTap.java
@@ -0,0 +1,14 @@
+package edu.uef.doan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewBaiTap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_bai_tap);
+    }
+}
